8d9068d6cde4a3eef11f65ccf48cf121d94dd6f8
Aditya Kumar <aditya5kumar2012@gmail.com>
2022-03-28 08:28:21 +0530

    multiply added

--- add.java
+++ add.java
@@ -3,6 +3,7 @@ public class add {
     public static void main(String[] args) {
        
         System.out.println("this is add function");
+        System.out.println("this is multiply function");
     }
 
 }
